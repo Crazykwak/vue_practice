7d175e853997736516467646485a12e45afc11e3
Crazykwak <kwj1830@naver.com>
2023-03-01 02:41:00 +0900

    test: v-for, v-if, methods, 상태관리, 이미지 주소 적용

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,69 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <h4> {{ products[0] }}</h4>
-      <p>{{ prices[0] }} 만원</p>
+
+    <div class="black-bg" v-if="isOpenModal" @click="modalSwitch">
+      <div class="white-bg">
+        <h4>{{ modal.title }}</h4>
+        <p>{{ modal.body}}</p>
+        <p>가격 : {{ modal.price }} 만원</p>
+        <button @click="modalSwitch"> 닫기 </button>
+      </div>
     </div>
 
-    <div>
-      <h4> {{ products[1] }}</h4>
-      <p>{{ prices[1] }} 만원</p>
+    <div class="menu">
+      <a href="">Home</a>
+      <a href="">Products</a>
+      <a href="">About</a>
     </div>
-
-    <div>
-      <h4> {{ products[2] }}</h4>
-      <p>{{ prices[2] }} 만원</p>
+    <div v-for="(product, index) in products" :key="product.id">
+      <img :src="product.img" alt="대표사진" class="room-img">
+      <h4 @click="[getNameAndBodyForModal(index), modalSwitch()]">
+        {{ product.id }} : {{ product.name }}</h4>
+      <p> {{ product.price }} 만원</p>
+      <button @click="increase(index)"> 허위 매물 신고 </button> <span> 신고수 : {{ product.alert }}</span>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  title: 'App',
   data() {
     return {
-      products: ['역삼동원룸', '천호동원룸', '구미동원룸'],
-      prices: [50, 60, 70],
+      isOpenModal: false,
+      modal: {
+        title: '',
+        body: '',
+        price: 0,
+      },
+      products: [
+        /* eslint-disable global-require */
+        { id: 1, img: require('./assets/room0.jpg'), name: '역삼동원룸', price: 50, body: '역삼동에 있는 원룸입니다.', alert: 0 },
+        { id: 2, img: require('./assets/room1.jpg'), name: '천호동원룸', price: 60, body: '천호동에 있는 원룸입니다.', alert: 0 },
+        { id: 3, img: require('./assets/room2.jpg'), name: '구미동원룸', price: 70, body: '구미동에 있는 원룸입니다.', alert: 0 },
+      ],
+      /* eslint-disable global-require */
     };
   },
+  methods: {
+    increase(index) {
+      this.products[index].alert += 1;
+    },
+    modalSwitch() {
+      this.isOpenModal = !this.isOpenModal;
+    },
+    getNameAndBodyForModal(index) {
+      this.modal.title = this.products[index].name;
+      this.modal.body = this.products[index].body;
+      this.modal.price = this.products[index].price;
+    },
+  },
+  components: {
+
+  },
 };
+
 </script>
 
 <style>
@@ -38,6 +73,42 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 </style>
