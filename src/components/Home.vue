eb19f927d243be3146e837e2bc82853474ae50f1
Crazykwak <kwj1830@naver.com>
2023-03-07 22:58:00 +0900

    feat: local spring 서버랑 통신: Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,8 @@
     <h1> 안녕 여긴 내 스케치북이야 </h1>
     <h2> 기능 구현 하고 싶은걸 여기에 하나하나 추가할까해 </h2>
     <h2> 지금은 작지만 좋은 아이디어가 떠오르면 여긴 멋져질거야 </h2>
+    <h1> 이 구역은 Ajax 통신을 위한 공간이야!</h1>
+    <h1> {{ msg }}</h1>
   </div>
 </template>
 
@@ -11,8 +13,19 @@ export default {
   name: 'home',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: '통신 안돼써!!',
     };
+  },
+  methods: {
+    getHomeMsg() {
+      this.$axios.get(this.$url)
+        .then((response) => {
+          this.msg = response.data;
+        });
+    },
+  },
+  created() {
+    this.getHomeMsg();
   },
 };
 </script>
